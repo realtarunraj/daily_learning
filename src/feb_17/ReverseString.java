edd3f4dd08e1cc23e69d800af5513f551720c886
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2026-02-17 22:43:56 +0530

    Initial Commit

---
+++ src/feb_17/ReverseString.java
@@ -0,0 +1,14 @@
+package feb_17;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		String str = "REALTARUNRAJ";
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = str.length() - 1; i >= 0; i--) {
+			sb.append(str.charAt(i));
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
